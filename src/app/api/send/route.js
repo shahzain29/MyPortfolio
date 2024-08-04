405579ef9394114249833cc6a1b87e23d9839514
Shahzain Sohail <105819846+shahzain29@users.noreply.github.com>
2024-08-04 17:46:59 +0100

    api key added

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_T3JwvU8a_FpXde1GsuAjsqW29CuMQcfT9');
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
