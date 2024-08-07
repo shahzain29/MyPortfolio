7346800ea021790ed0ec6645bf796de86365d025
Shahzain Sohail <105819846+shahzain29@users.noreply.github.com>
2024-08-07 23:03:51 +0100

    contact section changes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span className="text-blue-500 font-semibold text-2xl">SS</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <div>
+        <p className="text-slate-600">Email: shahzainsohail29@gmail.com</p>
+        <p className="text-slate-600">Mobile: +447459515444</p>
+        </div>
       </div>
     </footer>
   );
